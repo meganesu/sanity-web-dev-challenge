ce6f3c9bf56f10d7c06c262efff7b7871b0370fc
Megan Sullivan <3609989+meganesu@users.noreply.github.com>
2024-10-23 11:19:51 -0700

    feat: set up sanity studio and react frontend

---
+++ react-frontend/src/App.jsx
@@ -0,0 +1,20 @@
+import './App.css'
+import TeamBuilder from './TeamBuilder'
+
+function App() {
+  return (
+    <div id="content">
+      <header>
+        <p>Title</p>
+      </header>
+      <main>
+        <TeamBuilder />
+      </main>
+      <footer>
+        This was created as part of the Sanity Web Dev Challenge Hackathon
+      </footer>
+    </div>
+  )
+}
+
+export default App
---
+++ react-frontend/src/TeamBuilder.jsx
@@ -0,0 +1,9 @@
+const TeamBuilder = () => {
+  return (
+    <>
+      <p>Let's build a reindeer team!</p>
+    </>
+  )
+}
+
+export default TeamBuilder;
